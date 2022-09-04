1a270e7730fcf44ed1b5c1d33715fa2051c022fc
NiRob BarMan <47166768+rsnirob@users.noreply.github.com>
2022-09-04 13:01:29 +0600

    Create 2.4 simple if.cpp

---
+++ প্রোগ্রামিং কনটেস্ট ডেটা স্ট্রাকচার ও অ্যালগরিদম/অধ্যায় ০২/C ঝালাই/2.3 if - else if - else/2.4 simple if.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    if(condition 1)
+    {
+        /// if condition 1 is true;
+    }
+    else if(condition 2)
+    {
+        ///otherwise, if condition 2 is true;
+    }
+    else
+    {
+        ///if no condition is true;
+    }
+}
