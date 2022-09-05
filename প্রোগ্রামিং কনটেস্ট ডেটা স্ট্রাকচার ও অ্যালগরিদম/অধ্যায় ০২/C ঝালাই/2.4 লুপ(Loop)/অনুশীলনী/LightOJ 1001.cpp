fa1290d806839b04ed8f3a30aab388fba74f5870
NiRob BarMan <47166768+rsnirob@users.noreply.github.com>
2022-09-05 19:23:00 +0600

    Create LightOJ 1001.cpp

---
+++ প্রোগ্রামিং কনটেস্ট ডেটা স্ট্রাকচার ও অ্যালগরিদম/অধ্যায় ০২/C ঝালাই/2.4 লুপ(Loop)/অনুশীলনী/LightOJ 1001.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+
+        if(n>10)
+            printf("%d %d\n",10,n-10);
+        else
+            printf("%d %d\n",0,n);
+    }
+}
