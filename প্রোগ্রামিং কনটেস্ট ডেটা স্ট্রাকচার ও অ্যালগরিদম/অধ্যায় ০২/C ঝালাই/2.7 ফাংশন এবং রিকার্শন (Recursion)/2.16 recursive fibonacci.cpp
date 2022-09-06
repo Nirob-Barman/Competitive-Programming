83114067f8fbeb66f0ae0e615e50eb0fb1be240e
NiRob BarMan <47166768+rsnirob@users.noreply.github.com>
2022-09-06 13:16:59 +0600

    Create 2.16 recursive fibonacci.cpp

---
+++ প্রোগ্রামিং কনটেস্ট ডেটা স্ট্রাকচার ও অ্যালগরিদম/অধ্যায় ০২/C ঝালাই/2.7 ফাংশন এবং রিকার্শন (Recursion)/2.16 recursive fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int Fibonacci(int n)
+{
+    if(n<=1)
+        return n;
+    return Fibonacci(n-1)+Fibonacci(n-2);
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    printf("Nth Fibonacci = %d\n",Fibonacci(n));
+}
