3b60ac293dc9a3b66e2437b8259ebab6ff52cbd5
NiRob BarMan <47166768+rsnirob@users.noreply.github.com>
2022-09-06 13:07:34 +0600

    Create 2.14 fibonacci.cpp

---
+++ প্রোগ্রামিং কনটেস্ট ডেটা স্ট্রাকচার ও অ্যালগরিদম/অধ্যায় ০২/C ঝালাই/2.6 টাইম কমপ্লেক্সিটি (Time Copmplexity) এবং মেমোরী কমপ্লেক্সিটি (Memory Complexity)/2.14 fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    int a=0,b=1,c;
+    for(int i=2;i<=n;i++)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    printf("Nth Fibonacci = %d\n",b);
+}
