8f4549dc8b54b8dda99e1808e81b122f77fa5a1b
NiRob BarMan <47166768+rsnirob@users.noreply.github.com>
2022-09-10 19:58:24 +0600

    Update Timus 1068.cpp

--- প্রোগ্রামিং কনটেস্ট ডেটা স্ট্রাকচার ও অ্যালগরিদম/অধ্যায় ০২/C ঝালাই/2.3 if - else if - else/অনুশীলনী/Timus 1068.cpp
+++ প্রোগ্রামিং কনটেস্ট ডেটা স্ট্রাকচার ও অ্যালগরিদম/অধ্যায় ০২/C ঝালাই/2.3 if - else if - else/অনুশীলনী/Timus 1068.cpp
@@ -5,11 +5,10 @@ int main()
     int n;
     scanf("%d",&n);
 
-    if(n>=0)
+    if(n>0)
         printf("%d\n",n*(n+1)/2);
-
-    if(n<0)
+    else
         printf("%d\n",1+(n*(1-n)/2));
-    
+
     return 0;
 }
