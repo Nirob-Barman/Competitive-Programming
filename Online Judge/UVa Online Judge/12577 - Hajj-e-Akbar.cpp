2ee3f545a86554e04cc74ba1aad49fca75902050
NiRob BarMan <47166768+rsnirob@users.noreply.github.com>
2022-09-11 14:56:07 +0600

    Create 12577 - Hajj-e-Akbar.cpp

---
+++ Online Judge/UVa Online Judge/12577 - Hajj-e-Akbar.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char s[10];
+    int k=1;
+    while(scanf("%s",s)!=EOF)
+    {
+        if(!strcmp(s,"Hajj"))
+            printf("Case %d: Hajj-e-Akbar\n",k++);
+        else if(!strcmp(s,"Umrah"))
+            printf("Case %d: Hajj-e-Asghar\n",k++);
+        else if(!strcmp(s,"*"))
+        break;
+    }
+}
